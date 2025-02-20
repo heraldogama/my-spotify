876c55c6aa2243c5475f5757edf0ea24eb17b5f0
Heraldo Gama <heraldogama@gmail.com>
2025-02-20 16:02:47 -0300

    first commit, of the project in ReactJS.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import "./index.css";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
