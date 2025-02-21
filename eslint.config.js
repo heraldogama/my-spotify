5a39498c63dc997427b99f2ae24aecc4a3763af8
Heraldo Gama <heraldogama@gmail.com>
2025-02-21 18:25:39 -0300

    Implement the components ItemList and SingleItem of the Home Page

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ export default [
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2021,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: "latest",
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import SingleItem from "./SingleItem";
+// import { artistArray } from "../assets/database/artists.js";
 
-const ItemList = () => {
-  return <div></div>;
+const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+  return (
+    <div className="item-list">
+      <div className="item-list__header">
+        <h2>{title} Populares</h2>
+        <a href={path} className="item-list__link">
+          Mostrar tudo
+        </a>
+      </div>
+      <div className="item-list__container">
+        {itemsArray
+          .filter((currentValue, index) => index < items)
+          .map((currentObject, index) => (
+            <SingleItem
+              idPath={idPath}
+              {...currentObject}
+              key={`${title}-${index}`}
+            />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default ItemList;
